badc4bb4fd3f279a07da585e2a94ecf690db9fe6
facutorraca <ftorraca@fi.uba.ar>
2020-12-22 21:39:19 -0300

    fix fastify listen

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8080;
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(port);
+    await fastify.listen(port, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
