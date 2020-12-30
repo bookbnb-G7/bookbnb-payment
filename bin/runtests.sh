4e8574a96e56655988ce213cf5d2e8f1d50fd9c3
facutorraca <ftorraca@fi.uba.ar>
2020-12-30 00:32:15 -0300

    remove package-json

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 ENVIRONMENT=testing docker-compose up --build -d
 docker exec bookbnb-payment_web bash -c 'while !</dev/tcp/db/5432; do sleep 1; done;'
 docker exec bookbnb-payment_web npm test
