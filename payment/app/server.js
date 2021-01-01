a8380a90089dab64fe7d91792a7346fa5db14280
nicofandos <nfandos@fi.uba.ar>
2021-01-01 20:53:47 -0300

    Added swagger, example in 'createIntentBookHandler'

--- payment/app/server.js
+++ payment/app/server.js
@@ -6,6 +6,12 @@ const controllers = require('./controllers/controllers')({ config });
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true });
 
+// Import Swagger Options
+const swagger = require('./swagger')
+
+// Register Swagger
+fastify.register(require('fastify-swagger'), swagger.options)
+
 // Declares routes
 routes.forEach((route) => fastify.route(route({ config, controllers })));
 
@@ -13,6 +19,7 @@ routes.forEach((route) => fastify.route(route({ config, controllers })));
 database.sync().then(async () => {
     try {
       await fastify.listen(process.env.PORT || 8080, '0.0.0.0');
+      fastify.swagger();
       fastify.log.info(`server listening on ${fastify.server.address().port}`);
     } catch (err) {
       fastify.log.error(err);
