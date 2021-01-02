9cd30bb1f2612a6cd7a5881cc3e339a8d2a30eaf
facutorraca <ftorraca@gmail.com>
2021-01-02 11:23:30 -0300

    fix fuck'

--- payment/app/config.js
+++ payment/app/config.js
@@ -1,6 +1,6 @@
-const URL_NODE = 'https://kovan.infura.io/v3/a6e4d308c9e0435394de58930c9fabe6'//process.env.URL_NODE || '';
-const URL_WSKT = 'wss://kovan.infura.io/ws/v3/a6e4d308c9e0435394de58930c9fabe6' //process.env.URL_WSKT || '';
-const CTC_ADDR = '0x82C39Ec55980Bce3CE4164c0De4fc6e27a7d3F28' //process.env.CTC_ADDR || '';
+const URL_NODE = process.env.URL_NODE || '';
+const URL_WSKT = process.env.URL_WSKT || '';
+const CTC_ADDR = process.env.CTC_ADDR || '';
 
 const config = {
   urlNode: URL_NODE,
